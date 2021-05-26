b4eb1a63faf52c26c9da3fcff24965a2bcd55249
joiellantero <jtllantero@up.edu.ph>
2021-05-26 22:15:00 +0800

    input redirection and fix bug at long division function

--- mp01.py
+++ mp01.py
@@ -10,12 +10,10 @@ def get_c(data):
 def generate_alpha(i, alpha, a_list):
     if i == 256:
         return alpha
-
     elif i < 8:
         alpha = 2**i
         a_list.append(alpha)
         return generate_alpha(i+1, alpha, a_list)
-
     elif i >= 8:
         alpha *= 2
         if alpha >= 256:
@@ -47,29 +45,35 @@ def long_division(mx, a_list, gx, c, n):
         gx.append(0)
     while len(rx) > c:
         h = rx[0]
+        if h == 0:
+            del rx[0]
+            del gx[-1]
+            continue
         gh = gx.copy()
         for i in range(c+1):
             gh[i] = a_list[(gh[i] + a_list.index(h))%255]
         for i in range(c+1):
             rx[i] = gh[i]^rx[i]
-        del rx[0]
-        del gx[-1]
     return rx
 
 
 if __name__ == '__main__':
-    with open('testcase.txt') as f:
-        lines = f.readlines()
-        lines = [i.replace('\n', '') for i in lines]
-        alpha_list = []
-        msg_list = []
-        data = []
-        for i in range(1, len(lines), 1):
-            if 2 < len(lines[i]) < 8:
-                vmn = lines[i].split(' ')
-                data.append(get_c(vmn))
-                continue
-            msg_list.append(lines[i])
-        generate_alpha(0, 0, alpha_list)
-        for i in range(len(msg_list)):
-            print(long_division(msg_list[i], alpha_list, polynomial_generator(alpha_list, data[i]['c']), data[i]['c'], data[i]['N']))
+    T = input()
+    lines = []
+    for i in range(int(T)*2):
+        lines.append(input())
+    lines = [i.replace('\n', '') for i in lines]
+    alpha_list = []
+    msg_list = []
+    data = []
+    for i in range(0, len(lines), 1):
+        if 2 < len(lines[i]) < 8:
+            vmn = lines[i].split(' ')
+            data.append(get_c(vmn))
+            continue
+        msg_list.append(lines[i])
+    generate_alpha(0, 0, alpha_list)
+    for i in range(len(msg_list)):
+        temp = long_division(msg_list[i], alpha_list, polynomial_generator(alpha_list, data[i]['c']), data[i]['c'], data[i]['N'])
+        final = ' '.join(map(str, temp))
+        print(final)
